c132a963c434d5fc58ce3b9ea4ae58f6d06274fe
urirmz <ing.urirmz@gmail.com>
2023-12-14 20:17:12 -0600

    Timeline

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -16,9 +16,9 @@ function showRemaining() {
     var hours = Math.floor((distance % _day) / _hour);
     var minutes = Math.floor((distance % _hour) / _minute);
     var seconds = Math.floor((distance % _minute) / _second);
-    document.getElementById('countdown').innerHTML = days + 'days ';
-    document.getElementById('countdown').innerHTML += hours + 'hrs ';
-    document.getElementById('countdown').innerHTML += minutes + 'mins ';
-    document.getElementById('countdown').innerHTML += seconds + 'secs';
+    document.getElementById('countdown').innerHTML = days + ' días ';
+    document.getElementById('countdown').innerHTML += hours + ' hrs ';
+    document.getElementById('countdown').innerHTML += minutes + ' mins ';
+    document.getElementById('countdown').innerHTML += seconds + ' segs';
 }
 timer = setInterval(showRemaining, 1000);
