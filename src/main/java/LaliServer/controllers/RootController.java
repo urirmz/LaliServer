97a1ef1a7ef185fb3e43381a0edc74347b411775
urirmz <ing.urirmz@gmail.com>
2024-03-19 23:16:51 -0600

    fix: LaliServer folders and tweaks

--- src/main/java/LaliServer/controllers/RootController.java
+++ src/main/java/LaliServer/controllers/RootController.java
@@ -1,5 +1,6 @@
-package LaliServer;
+package LaliServer.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
